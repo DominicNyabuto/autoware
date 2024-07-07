661ec3337d1e8fafc98fcf1e0e42a40dbd2b2c9b
Dominic Nyabuto <dominicnyabuto@outlook.com>
2024-07-07 19:07:36 +0300

    Add files via upload

---
+++ VS-Ubuntu/C-beginner/hello.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    // printf("thank you, world\n");
+
+    // printf("thank you, world\n");
+    /* this is a multi-line comment -  this 
+                                        is 
+                                        a multi-line 
+                                        cnnnomment 
+    */
+
+    // printf("\"To live is to suffer\" - Fredrick Nietzsche\n");
+ 
+    // int x; //declaration 
+    // x = 1234; //initialization 
+    // int y = 321; // declaration + initialization 
+
+    // int age = 21; //integer 
+    // float gpa = 3.61;  // float
+    // double marks = 57.78967979; // %1f 
+    // char grade = 'C'; //single character 
+    // char name[] = "Dominic"; //array of characters
+
+    // // bool e = false; 
+
+    // // int sum = age + gpa;
+
+    // // printf("The sum is %d\n", sum); 
+    // // printf("Hallo, %s\n", name);
+    // // printf("My gpa is %lf\n", marks);
+    // // printf("My gpa is %0.15f", marks);
+
+
+    // char f = 120; //each contains 1 byte (-128 to +127) %d or %c
+    // unsigned char g = 255; //1 byte each (0 to 255) %d or %c
+
+    // short int h = 32767; //store 2 bytes of memory each (-32767 to +32757)%d
+    // unsigned short int i = 65535; //2 bytes (0 to +65535)%d
+   
+    // int j = 213783647; // 4 bytes (-213783647 to +213783647) %d
+    // unsigned int k = 4245252525; // 4 bytes (0 to 4245252525) %u
+   
+    // long long int s = 213783647213783647; // 8 bytes (-9 quintillion to +9 quintillion) %lld
+    // unsigned long long int y = 424525252542452; // 8 bytes (0 to +18 quintillion ) %llu
+   
+
+
+
+
+    // constant - cn't be alterd by prpgram 
+
+    // const float PI = 3.14159; //makes PI read-only, 
+    //                           //cant be changed, h
+    //                           //hence error 
+    //                           //"assignment of read-only variable ‘PI’"
+    
+    // // PI = 3.14159; // results in the above error 
+
+    // printf("%f", PI); 
+
+
+
+
+
+    return 0; 
+
+}
